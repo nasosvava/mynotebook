4f9057c86c82ef7c370dd63fcaabee447717498b
nasosvava <78432133+nasosvava@users.noreply.github.com>
2022-01-19 19:03:33 +0200

    Initial commit

---
+++ src/main/java/com/example/mynotebook/domain/User.java
@@ -0,0 +1,4 @@
+package com.example.mynotebook.domain;
+
+public class User {
+}
---
+++ src/test/java/com/example/mynotebook/MynotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
